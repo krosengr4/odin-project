e66b249560ae3425e4c5345b96d6394fcb65e708
Kevin Rosengren <rosenkev4@gmail.com>
2026-05-06 12:41:49 -0500

    Completed assignment on basics of javascript operations, variables, and numbers

---
+++ 04-js-basics/assignments/variables-and-operators/script.js
@@ -0,0 +1,24 @@
+// 1 basic operations
+console.log("BASIC OPERATIONS");
+console.log(23 + 97);
+// 2 basic operations
+console.log(75 + 46 + 167 + 83 + 2 + 8);
+//3 basic operations
+console.log((4 + 6 + 9) / 77);
+
+// 4 variables
+console.log("VARIABLES");
+let a = 10;
+console.log(a);
+a = 5;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+// 5 Try this sequence
+console.log("SEQUENCE 1");
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
