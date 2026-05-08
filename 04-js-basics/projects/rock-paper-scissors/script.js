de99fecd98888733b693a929d87a83ed2e652fb9
Kevin Rosengren <rosenkev4@gmail.com>
2026-05-08 02:33:30 -0500

    Initial commits for rock paper scissor project. Added pseudo coding for this project in script.js file

---
+++ 04-js-basics/projects/rock-paper-scissors/script.js
@@ -0,0 +1,13 @@
+console.log(
+    "WELCOME TO ROCK-PAPER-SCISSORS OF DOOM! \nThink you're good enough to win? Prove it.",
+);
+
+/*
+ * Pseudocode:
+ * Get users choice.
+ *      User can choose 1 for rock, 2 for paper, or 3 for scissors.
+ * Get computers choice.
+ *      Use Math.random() to get a number from 1 - 3.
+ *      Convert number into a string, 1 rock 2 paper 3 scissors.
+ * Compare the user and computer choice and use the rock paper scissors rules to determine winner.
+ */
