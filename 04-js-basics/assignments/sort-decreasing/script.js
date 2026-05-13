d7b88910da1b0e084ce703a03a19e995fbf02383
Kevin Rosengren <rosenkev4@gmail.com>
2026-05-13 10:07:49 -0500

    Completed assignment to sort an array of integers in highest to lowest value order

---
+++ 04-js-basics/assignments/sort-decreasing/script.js
@@ -0,0 +1,8 @@
+// Given an array on integers, sort and return the array from highest value to
+// lowest
+
+let arr = [5, 2, 1, -10, 8];
+
+sortedArr = arr.sort((a, b) => a - b);
+
+console.log(sortedArr);
