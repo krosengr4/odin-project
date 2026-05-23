e1ab6591fb9964cfbe4c3dddd3a059d2ca477660
Kevin Rosengren <rosenkev4@gmail.com>
2026-05-23 16:13:57 -0500

    Completed sorting objects exercise in javascript.
    Write the function sortByAge(users) that gets an array of objects with
    the age property and sorts them by age.

---
+++ 04-js-basics/assignments/sort-by-age/script.js
@@ -0,0 +1,16 @@
+// Write the function sortByAge(users) that gets an array of objects with the age property and
+// sorts them by age.
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [pete, john, mary];
+console.log(arr);
+
+function sortByAge(arr) {
+    arr.sort((a, b) => a.age - b.age);
+}
+
+sortByAge(arr);
+console.log(arr);
