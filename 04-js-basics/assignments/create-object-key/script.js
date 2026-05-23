80ba08785975f8dfa3c4370417be7b6dd2df9b7f
Kevin Rosengren <rosenkev4@gmail.com>
2026-05-23 17:48:59 -0500

    Completed javascript array mapping exercise.
    Create a function groupById(arr) that creates an object from it, with id
    as the key, and array items as values.

---
+++ 04-js-basics/assignments/create-object-key/script.js
@@ -0,0 +1,22 @@
+/*
+ * Given an array of users in the form {id:..., name:..., age:... }.
+ *
+ * Create a function groupById(arr) that creates an object from it, with id as the key, and array
+ * items as values.
+ */
+
+let users = [
+    { id: "john", name: "John Smith", age: 20 },
+    { id: "ann", name: "Ann Smith", age: 24 },
+    { id: "pete", name: "Pete Peterson", age: 31 },
+];
+
+function groupById(arr) {
+    return arr.reduce((obj, value) => {
+        obj[value.id] = value;
+        return obj;
+    }, {});
+}
+
+let usersById = groupById(users);
+console.log(usersById);
