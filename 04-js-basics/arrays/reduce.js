2687fabd16db116a23a1fb20bbb3442281bbfd4a
Kevin Rosengren <rosenkev4@gmail.com>
2026-05-24 15:36:46 -0500

    Added notes on the parameters that the .reduce() array method takes

--- 04-js-basics/arrays/reduce.js
+++ 04-js-basics/arrays/reduce.js
@@ -2,10 +2,16 @@
  * The .reduce() method will iterate through an array and uses a "reducer"
  * function to condense all elements into a single output value.
  *
- * This callback function takes two parameters:
- * 1. The accumulator (current value of the result at that point in the loop).
- *      This will be set to the first element in the array if no initialValue is provided.
- * 2. The item (or array) currently being iterated on.
+ * Parameters:
+ * 1. (Required) accumulator / total:
+ *          (current value of the result at that point in the loop).
+ *          This will be set to the first element in the array if no initialValue is provided.
+ * 2. (Required) currentValue:
+ *          The item (or array) currently being iterated on.
+ * 3. (Optional) currentIndex:
+ *          The index position of currentValue in the array.
+ * 4. (Optional) array:
+ *          The array the current element belongs to.
  */
 
 const nums = [1, 2, 3, 4, 5];
