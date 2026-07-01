167dfae45f0c6103a618ab9d7fc4a43b4f2812ea
Kevin Rosengren <rosenkev4@gmail.com>
2026-07-01 06:08:39 -0500

    Added logic to display all books in myLibrary array. Added logic to delete book from array and display.

--- 07-javascript/organizing-with-objects/projects/library/script.js
+++ 07-javascript/organizing-with-objects/projects/library/script.js
@@ -3,10 +3,11 @@ const sumbitBtn = document.getElementById("submit-btn");
 const bookTitleEl = document.getElementById("book-title");
 const bookAuthorEl = document.getElementById("book-author");
 const pageCountEl = document.getElementById("page-count");
+const bookCards = document.querySelector(".books");
 
 sumbitBtn.addEventListener("click", addBookToLibrary);
 
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages) {
     this.title = title;
@@ -39,12 +40,14 @@ function addBookToLibrary() {
         return;
     }
 
-    let book = new Book(titleVal, authorVal, pagesVal);
+    let book = new Book(titleVal, authorVal, pagesNum);
     const bookValues = book.info();
     console.log(bookValues);
 
     myLibrary.push(book);
     closeDialog();
+
+    displayLibrary();
 }
 
 function closeDialog() {
@@ -54,11 +57,49 @@ function closeDialog() {
     addBookModal.close();
 }
 
+function deleteBook(book) {
+    myLibrary = myLibrary.filter((b) => b.uuid !== book.uuid);
+    displayLibrary();
+}
+
 function displayLibrary() {
+    bookCards.innerHTML = "";
+
     for (const book of myLibrary) {
-        const bookInfo = book.info();
-        console.log(bookInfo);
+        const bookCard = document.createElement("div");
+        bookCard.className = "card";
+
+        const titleHeader = document.createElement("h3");
+        titleHeader.className = "card-title";
+        titleHeader.textContent = book.title;
+        bookCard.appendChild(titleHeader);
+
+        const authorP = document.createElement("p");
+        authorP.className = "author";
+        authorP.textContent = book.author;
+        bookCard.appendChild(authorP);
+
+        const pageCountP = document.createElement("p");
+        pageCountP.className = "page-count";
+        pageCountP.textContent = `Page Count: ${book.pages}`;
+        bookCard.appendChild(pageCountP);
+
+        const uuidP = document.createElement("p");
+        uuidP.className = "uuid";
+        uuidP.textContent = `UUID: ${book.uuid}`;
+        bookCard.appendChild(uuidP);
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.className = "delete";
+        deleteBtn.textContent = "Delete Book";
+        bookCard.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener("click", () => deleteBook(book));
+
+        bookCards.appendChild(bookCard);
     }
 }
 
-displayLibrary(myLibrary);
+let greatGatsbyBook = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+myLibrary.push(greatGatsbyBook);
+displayLibrary();
