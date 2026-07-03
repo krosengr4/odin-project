6a0f13784be444e2a1af0986158a59e002bc9773
Kevin Rosengren <rosenkev4@gmail.com>
2026-07-03 18:10:32 -0500

    Pseudo code for a version of tictactoe that can be shown in the console.
    3 main factory functions for creating objects:
    - GameBoard
    - Square
    - GameController
    All properties (functions and variables) that each factory will provide.

---
+++ 07-javascript/organizing-with-objects/projects/tic-tac-toe/script.js
@@ -0,0 +1,28 @@
+/*
+ * Gameboard.
+ * Represents the state of the tic tac toe board and its 9 squares..
+ * An array is used with the 9 squares as getValue.
+ * getBoard()
+ * markSquare()
+ * printBoard() for console version
+ */
+
+/*
+ * Square.
+ * Square object represents a single square on the board.
+ * markSquare(player)
+ * getValue()
+ *
+ * Each square can have these values:
+ * 0 = No marking
+ * 1 = X (for player 1s marking)
+ * 2 = O (for player 2s marking)
+ */
+
+/*
+ * GameController.
+ * Controls the flow and state of players turns and checks for wins and ties.
+ * Array with both player objects containing name and mark(1=X 2=O)
+ * switchPlayerTurn()
+ * printNewRound() reprint board with changes.
+ */
